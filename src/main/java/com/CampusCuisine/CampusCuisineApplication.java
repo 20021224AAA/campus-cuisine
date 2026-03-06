4eb79436babf5cde868437e029578241dae6c622
20021224AAA <97327010+20021224AAA@users.noreply.github.com>
2026-03-06 13:58:38 +0800

    Update CampusCuisineApplication.java

--- src/main/java/com/CampusCuisine/CampusCuisineApplication.java
+++ src/main/java/com/CampusCuisine/CampusCuisineApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.hmdp.mapper")
+@MapperScan("com.CampusCuisine.mapper")
 @SpringBootApplication
 public class CampusCuisineApplication {
 
@@ -13,3 +13,4 @@ public class CampusCuisineApplication {
     }
 
 }
+
